5f8267b6c9aa73079a7f378046d648aa200a5719
Zaroon25 <zaroonbutt87@gmail.com>
2024-12-15 00:20:43 +0500

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,17 @@
+name = "Madhav"
+for a in name:
+    print(a)
+    
+for i in range(0 , 55 , 5):
+    print(i)
+    
+q = ["Smart" , "Funny" , "Cool"]
+n = ["Aalia" , "Madhav" , "Sabaoon"]
+for x in n:
+    for y in q :
+        print(x , y)
+        
+i = 1
+while i < 10:
+    print(i)
+    i += 1
